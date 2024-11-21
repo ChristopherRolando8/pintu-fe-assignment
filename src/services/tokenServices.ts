dacf1cdb3ac5143d2d9695594f5a9fcfd80c3ebf
ChristopherRolando8 <christopher.rolando@fazzfinancial.com>
2024-11-21 22:48:37 +0700

    fix: fix eslint error

--- src/services/tokenServices.ts
+++ src/services/tokenServices.ts
@@ -1,5 +1,6 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import axios from 'axios';
-import { Token } from '../types/token';
+import { Token } from '@/types/token';
 
 const API_URL_TOKENS = '/api/tokens';
 const API_URL_LOGOS = '/api/supportedCurrencies';
